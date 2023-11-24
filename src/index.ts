1f415e6090653403413bdbf152c99063c557c626
Matthew Wolfe <matthew.wolfe2@gmail.com>
2023-11-23 16:15:43 -0800

    patch: initial publish

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,13 @@ export {
 } from '@pkg/declarations';
 
 export { parser } from '@pkg/parser';
+
+export type {
+  AugmentedDeclaration,
+  ClassDeclaration,
+  FunctionDeclaration,
+  InterfaceDeclaration,
+  PropertyDeclaration,
+  ReferenceDeclaration,
+  TypeAliasDeclaration,
+} from '@pkg/types/declarations';
