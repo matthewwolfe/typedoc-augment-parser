3a830e7ce591af983b80ee6090a7b0af3b31a400
Matthew Wolfe <matthew.wolfe2@gmail.com>
2023-12-19 17:14:47 -0800

    bump dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
+import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     alias: {
-      '@pkg': './src',
+      '@pkg': path.resolve('src'),
     },
   },
 });
